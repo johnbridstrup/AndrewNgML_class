e2e30feb5ae01faadce11f373dbc535194a93a66
john <john.bridstrup@gmail.com>
2021-01-13 12:51:29 -0800

    optional part of ex5 in subplots

--- ex5/functions.py
+++ ex5/functions.py
@@ -61,3 +61,13 @@ def plotFit(min_x, max_x, mu, sigma, theta, p):
 
     X_poly = np.concatenate([np.ones((x.shape[0], 1)), X_poly], axis=1)
     plt.plot(x, np.dot(X_poly, theta), '--', lw=2)
+
+def plotFit_ax(min_x, max_x, mu, sigma, theta, p, ax):
+    # make range a little bigger
+    x = np.arange(min_x - 15, max_x + 25, 0.05).reshape(-1, 1)
+    X_poly = polyFeatures(x, p)
+    X_poly -= mu.T
+    X_poly /= sigma
+
+    X_poly = np.concatenate([np.ones((x.shape[0], 1)), X_poly], axis=1)
+    ax.plot(x, np.dot(X_poly, theta), '--', lw=2)
