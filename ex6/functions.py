48e1ad525139a227f0cf7eb4aa661f31e40b4ef3
john <john.bridstrup@gmail.com>
2021-01-19 13:24:46 -0800

    sklearn with rbf kernel uses MUCH larger C than matlab examples

--- ex6/functions.py
+++ ex6/functions.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 from numpy.linalg import norm
+from sklearn import svm
+from mlxtend.plotting import plot_decision_regions
 
 def plotData(X,y):
     # plot y=1 as X and y=0 as O on scatter plot
@@ -17,44 +19,51 @@ def gaussianKernel(x1,x2, sigma=1.0):
 def linearKernel(x1,x2):
     return np.dot(x1,x2)
 
-def displayData(X,y, grid=False):
+def displayData(X,y, grid=False, clf=None):
     pos = y == 1
     neg = y == 0
     plt.plot(X[pos, 0], X[pos, 1], 'X', mew=1, ms=10, mec='k')
     plt.plot(X[neg, 0], X[neg, 1], 'o', mew=1, mfc='y', ms=10, mec='k')
     plt.grid(grid)
 
-def svmTrain(X, y, C, kernelFunction, tol=1e-3, max_passes=5, args=()):
+    if clf:
+        if clf.kernel == 'linear':
+            w = clf.coef_[0]
+            a = -w[0] / w[1]
+            xx = np.linspace(np.min(X[:,0]), np.max(X[:,0]), 10)
+            yy = a * xx - clf.intercept_[0] / w[1]
+            plt.plot(xx,yy,'k-')
+        elif clf.kernel == 'rbf':
+            x_min, x_max = X[:, 0].min(), X[:, 0].max()
+            y_min, y_max = X[:, 1].min(), X[:, 1].max()
+            plot_decision_regions(X, y, clf=clf, legend=2)
+            plt.xlim(x_min, x_max)
+            plt.ylim(y_min, y_max)
+            # h = 0.2 # Mesh step size
+            # cm = plt.cm.RdBu
+            # x_min, x_max = X[:, 0].min(), X[:, 0].max()
+            # y_min, y_max = X[:, 1].min(), X[:, 1].max()
+            # xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+            #              np.arange(y_min, y_max, h))
+            # if hasattr(clf, "decision_function"):
+            #     Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+            # else:
+            #     Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+            # Z = Z.reshape(xx.shape)
+            # plt.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+        else:
+            raise TypeError('Must be rbf or linear (for now)')
+
+    
+
+def svmTrain(X, y, C, kernel='linear'):
     y = y.astype(int)
-    m,n = X.shape
 
-    passes = 0
-    E = np.zeros(m)
-    alphas = np.zeros(m)
-    b = 0
+    # They want me to use a simplified model they wrote themselves
+    # Im just gonna use sklearn SVM packages...
 
-    # map y = 0 to y = -1 (Why do I have to do this?)
-    y[y==0] = -1
+    clf = svm.SVC(kernel=kernel, C = C)
+    clf.fit(X,y)
+    return clf
 
-    # Optimize linear and gaussian kernels
-    if kernelFunction.__name__ == 'linearKernel':
-        # Vectorized computation for the linear kernel
-        # This is equivalent to computing the kernel on every pair of examples
-        K = np.dot(X, X.T)
-    elif kernelFunction.__name__ == 'gaussianKernel':
-        # vectorized RBF Kernel
-        # This is equivalent to computing the kernel on every pair of examples
-        X2 = np.sum(X**2, axis=1)
-        K = X2 + X2[:, None] - 2 * np.dot(X, X.T)
-
-        if len(args) > 0:
-            K /= 2*args[0]**2
-
-        K = np.exp(-K)
-    else:
-        K = np.zeros((m, m))
-        for i in range(m):
-            for j in range(i, m):
-                K[i, j] = kernelFunction(X[i, :], X[j, :])
-                K[j, i] = K[i, j]
 
