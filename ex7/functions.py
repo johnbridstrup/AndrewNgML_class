26dfa201e91bd8bd6076e03a96548c5742a54301
john <john.bridstrup@gmail.com>
2021-01-21 17:03:17 -0800

    faces pca

--- ex7/functions.py
+++ ex7/functions.py
@@ -107,3 +107,43 @@ def projectData(X, U, K):
 def recoverData(Z, U, K):
     X_rec = Z.dot(U[:,:K].T)
     return X_rec
+
+def displayData(X, example_width=None, figsize=(10, 10)):
+    """
+    Displays 2D data in a nice grid.
+    Parameters
+    ----------
+    X : array_like
+        The input data of size (m x n) where m is the number of examples and n is the number of
+        features.
+    example_width : int, optional
+        THe width of each 2-D image in pixels. If not provided, the image is assumed to be square,
+        and the width is the floor of the square root of total number of pixels.
+    figsize : tuple, optional
+        A 2-element tuple indicating the width and height of figure in inches.
+    """
+    # Compute rows, cols
+    if X.ndim == 2:
+        m, n = X.shape
+    elif X.ndim == 1:
+        n = X.size
+        m = 1
+        X = X[None]  # Promote to a 2 dimensional array
+    else:
+        raise IndexError('Input X should be 1 or 2 dimensional.')
+
+    example_width = example_width or int(np.round(np.sqrt(n)))
+    example_height = int(n / example_width)
+
+    # Compute number of items to display
+    display_rows = int(np.floor(np.sqrt(m)))
+    display_cols = int(np.ceil(m / display_rows))
+
+    fig, ax_array = plt.subplots(display_rows, display_cols, figsize=figsize)
+    fig.subplots_adjust(wspace=0.025, hspace=0.025)
+
+    ax_array = [ax_array] if m == 1 else ax_array.ravel()
+
+    for i, ax in enumerate(ax_array):
+        ax.imshow(X[i].reshape(example_height, example_width, order='F'), cmap='gray')
+        ax.axis('off')
